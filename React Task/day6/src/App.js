04018714c88959752351765e01166f74ef3d7083
Sai Aravindh <saiaravindhchangepond27@gmail.com>
2024-12-03 18:30:14 +0530

    Day 6 Task

---
+++ React Task/day6/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CountComp from './Component/CountComp';
+
+function App() {
+  return (
+    <div className="App">
+        <CountComp />
+    </div>
+  );
+}
+
+export default App;

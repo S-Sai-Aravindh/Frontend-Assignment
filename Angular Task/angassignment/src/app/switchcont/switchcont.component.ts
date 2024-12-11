e62eb03783782c8dea3dc28561a68da75fa8302a
Sai Aravindh <saiaravindhchangepond27@gmail.com>
2024-12-11 19:08:17 +0530

    Angular task1.5

--- Angular Task/angassignment/src/app/switchcont/switchcont.component.ts
+++ Angular Task/angassignment/src/app/switchcont/switchcont.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SquarePipe } from '../shared/custompipe/square.pipe';
 
 @Component({
   selector: 'app-switchcont',
-  imports: [CommonModule],
+  imports: [CommonModule,SquarePipe],
   templateUrl: './switchcont.component.html',
   styleUrl: './switchcont.component.css'
 })
@@ -16,6 +17,7 @@ export class SwitchcontComponent {
   bike : string[] = ["Hero MotoCorp","Mahindra Two Wheelers","Royal Enfield","Bajaj Auto","TVS Motor Company"];
   car : string[] = ["Maruti Suzuki","Hyundai","Tata Motors","Mahindra","Honda"];
   values: string[] = ["Hero MotoCorp","Mahindra Two Wheelers","Royal Enfield","Bajaj Auto","TVS Motor Company"];
+  motor : string = "Hero MotoCorp";
 
   switch(vals:string){
     if(vals == "bike"){
@@ -28,4 +30,48 @@ export class SwitchcontComponent {
       this.values = this.car;
     }
   }
+
+  switch2(valuess : string){
+
+    if(valuess == "Hero MotoCorp")
+      {
+      this.motor = "Hero MotoCorp"
+    }
+    else if(valuess == "Mahindra Two Wheelers")
+      {
+      this.motor = "Mahindra Two Wheelers"
+    }
+    else if(valuess == "Royal Enfield")
+      {
+      this.motor = "Royal Enfield"
+    }
+    else if(valuess == "Bajaj Auto")
+      {
+      this.motor = "Bajaj Auto"
+    }
+    else if(valuess == "TVS Motor Company")
+      {
+      this.motor = "TVS Motor Company"
+    }
+    else if(valuess == "Maruti Suzuki")
+      {
+      this.motor = "Maruti Suzuki"
+    }
+    else if(valuess == "Hyundai")
+      {
+      this.motor = "Hyundai"
+    }
+    else if(valuess == "Tata Motors")
+      {
+      this.motor = "Tata Motors"
+    }
+    else if(valuess == "Mahindra")
+      {
+      this.motor = "Mahindra"
+    }
+    else if(valuess == "Honda")
+      {
+      this.motor = "Honda"
+    }
+}
 }
